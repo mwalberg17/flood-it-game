9618138847f0cb53176fc1103b9570eb6a27869b
Matthew Walberg <mwalberg17@gmail.com>
2018-12-26 19:41:56 -0600

    Added Game class to hold most of the logic for the game, instead of having it in UserInterface. Added comments as well.

--- Flood It Game/Game.cs
+++ Flood It Game/Game.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* Game.cs
+ * Author: Matthew Walberg
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,89 +9,132 @@ using System.Threading.Tasks;
 
 namespace Flood_It_Game
 {
+    /// <summary>
+    /// Game class to hold most logic for the flood-it game.
+    /// </summary>
     public class Game
     {
-        // 
+        /// <summary>
+        /// Size fo the rows and columns in the current game.
+        /// </summary>
+        private int _size;
 
+        /// <summary>
+        /// 2D array holding the color numeric values of each square.
+        /// </summary>
+        private int[,] _boardArray;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private int _gameRows;
-        private int _gameCols;
-        private int[,] _board;
-
-        public Game(int difficulty)
+        /// <summary>
+        /// Constructor for game class setting size, creating the board, and setting turns.
+        /// </summary>
+        /// <param name="size">Size of board</param>
+        public Game(int size)
         {
-            if (difficulty == 1)
-            {
-                Cols = 15;
-                Rows = 15;
-                CreateBoard();
-            }
-            else if (difficulty == 2)
-            {
-                Cols = 30;
-                Rows = 30;
-            }
-            else
-            {
-                Cols = 45;
-                Rows = 45;
-            }
+            _size = size;
+            CreateBoardArray();
+            TurnsAllowed();
         }
 
-        public int Rows { get; set; }
-        public int Cols { get; set; }
+        /// <summary>
+        /// Amount of allowed turns for the current game.
+        /// </summary>
+        public int Turns { get; set; }
 
-        public void CreateBoard()
+        /// <summary>
+        /// Fills 2D array with random numbers between 1 and 6 representing the color values.
+        /// </summary>
+        private void CreateBoardArray()
         {
-            _board = new int[Rows, Cols];
+            _boardArray = new int[_size, _size];
             Random rand = new Random();
-            for (int x = 0; x < Rows; x++)
+            for (int row = 0; row < _size; row++)
             {
-                for (int y = 0; y < Cols; y++)
+                for (int col = 0; col < _size; col++)
                 {
-                    _board[x, y] = rand.Next(1, 6);
+                    _boardArray[row, col] = rand.Next(1, 6);
                 }
             }
         }
-        /// <summary>
-        /// Checks the array representing the board for a win. Returns false if an unmatched
-        /// </summary>
-        /// <param name="board"></param>
-        /// <param name="rows">Number of rows for the current game.</param>
-        /// <param name="cols">Number of columns for the current game.</param>
-        /// <returns></returns>
-        public bool HasWon(int[,] board)
-        {
-            //Maybe instead of having this. Maybe keep a private counter in UserInterface that
-            // gets incremented everytime flood fill algorithm goes
 
-            //check if all corners are same and if so then do full check
-            for (int x = 0; x < Rows; x++)
+        /// <summary>
+        /// Sets Turns property for specific size of board.
+        /// </summary>
+        private void TurnsAllowed()
+        {
+            switch (_size)
             {
-                for (int y = 0; y < Cols; y++)
+                case 10:
+                    Turns = 17;
+                    break;
+                case 14:
+                    Turns = 25;
+                    break;
+                case 18:
+                    Turns = 32;
+                    break;
+                default:
+                    Turns = 100;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Returns 2D board.
+        /// </summary>
+        /// <returns>Board filled with numeric color values.</returns>
+        public int[,] GetBoard()
+        {
+            return _boardArray;
+        }
+
+        /// <summary>
+        /// Flood fill algorithm using recursion to change all squares with same color value of [0,0] that are
+        /// connected by cardinal directions to the new color.
+        /// </summary>
+        /// <param name="newColor"></param>
+        /// <param name="oldColor"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        public void FloodFill(int newColor, int oldColor, int row, int col)
+        {
+            if (_boardArray[row, col] != oldColor)
+            {
+                return;
+            }
+            _boardArray[row, col] = newColor;
+            if (row > 0)
+            {
+                FloodFill(newColor, oldColor, row - 1, col);
+            }
+            if (row < _size - 1)
+            {
+                FloodFill(newColor, oldColor, row + 1, col);
+            }
+            if (col > 0)
+            {
+                FloodFill(newColor, oldColor, row, col - 1);
+            }
+            if (col < _size - 1)
+            {
+                FloodFill(newColor, oldColor, row, col + 1);
+            }
+        }
+
+        /// <summary>
+        /// Checks if all squares are the same color, indicating a win.
+        /// </summary>
+        /// <returns>Whether the user has won the game.</returns>
+        public bool CheckWin()
+        {
+            if (_boardArray[_size - 1, _size - 1] != _boardArray[0, 0])
+            {
+                return false;
+            }
+            for (int x = 0; x < _size; x++)
+            {
+                for (int y = 0; y < _size; y++)
                 {
-                    if (board[x, y] != board[0, 0])
+                    if (_boardArray[x, y] != _boardArray[0, 0])
                     {
                         return false;
                     }
--- Flood It Game/UserInterface.cs
+++ Flood It Game/UserInterface.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* UserInterface.cs
+ * Author: Matthew Walberg
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,45 +14,45 @@ using System.Windows.Forms;
 namespace Flood_It_Game
 {
     public partial class UserInterface : Form
-    {
-        private const int _squareSize = 20; // Constant int to easily change the size of each square
-        private int _boardSize;             // Int to hold the size of the rows and columns of the current game
-        private int _turnsAllowed;          // Int representing the amount of turns allowed in the current game
-        private int _turn = 0;             // Int representing which turn the user is on
-        private int[,] _boardArray;        // 2-Dimensional array to hold numeric value of color
+    {   
+        /// <summary>
+        /// Constant int holding the size in pixels for each square
+        /// </summary>
+        private const int _squareSize = 20; 
 
+        /// <summary>
+        /// Size of rows and columns of current game
+        /// </summary>
+        private int _boardSize;
 
+        /// <summary>
+        /// Turn user is on
+        /// </summary>
+        private int _turn = 0;
+
+        /// <summary>
+        /// Creates game object
+        /// </summary>
+        private Game _game;
+        
+        /// <summary>
+        /// Create GUI and sets up game.
+        /// </summary>
         public UserInterface()
         {
             InitializeComponent();
             _boardSize = Convert.ToInt32(uxSizeList.Text.Substring(0, 2)); // Sets to value in drop down box
-            TurnsAllowed();                                                // Calls method to set _turnsAllowed int
-            CreateBoardArray();
-            DrawBoard();
-            uxTurns.Text = "Turns: " + _turn + "/" + _turnsAllowed;
+            _game = new Game(_boardSize);
+            DrawBoard(_game.GetBoard());
+            uxTurns.Text = "Turns: " + _turn + "/" + _game.Turns;
         }
-
-        private void CreateBoardArray()
+        
+        /// <summary>
+        /// Creates a new game board
+        /// </summary>
+        /// <param name="board">2D board with all the color values</param>
+        private void DrawBoard(int [,] board)
         {
-            _boardArray = new int[_boardSize, _boardSize];
-            Random rand = new Random();
-            for (int row = 0; row < _boardSize; row++)
-            {
-                for (int col = 0; col < _boardSize; col++)
-                {
-                    _boardArray[row, col] = rand.Next(1, 6);
-                }
-            }
-        }
-
-        private void DrawBoard()
-        {
-            if (_boardSize == 10)
-                uxBoard.Location = new Point(91, 118);
-            else if (_boardSize == 14)
-                uxBoard.Location = new Point(51, 78);
-            else
-                uxBoard.Location = new Point(11, 38);
             uxBoard.Width = _squareSize * _boardSize;
             uxBoard.Height = uxBoard.Width; 
             Padding pad = new Padding();
@@ -59,6 +62,18 @@ namespace Flood_It_Game
             {
                 uxBoard.Controls.Clear();
             }
+            if (_boardSize == 10)
+            {
+                uxBoard.Location = new Point(91, 118);
+            }
+            else if (_boardSize == 14)
+            {
+                uxBoard.Location = new Point(51, 78);
+            }
+            else
+            {
+                uxBoard.Location = new Point(11, 38);
+            }
             for (int row = 0; row < _boardSize; row++)
             {
                 for (int col = 0; col < _boardSize; col++)
@@ -67,7 +82,7 @@ namespace Flood_It_Game
                     square.Width  = _squareSize;
                     square.Height = _squareSize;
                     square.Margin = pad;
-                    int temp = _boardArray[row, col];
+                    int temp = board[row, col];
                     switch (temp)
                     {
                         case 1:
@@ -97,14 +112,18 @@ namespace Flood_It_Game
             }
         }
 
-        private void RedrawBoard()
+        /// <summary>
+        /// Redraws the board without creating a new one
+        /// </summary>
+        /// <param name="board">2D board with all the color values</param>
+        private void RedrawBoard(int [,] board)
         {
             for (int row = 0; row < _boardSize; row++)
             {
                 for (int col = 0; col < _boardSize; col++)
                 {
                     Label square = (Label)uxBoard.Controls[row + "," + col];
-                    int temp = _boardArray[row, col];
+                    int temp = board[row, col];
                     switch (temp)
                     {
                         case 1:
@@ -129,73 +148,36 @@ namespace Flood_It_Game
                 }
             }
         }
-
-        private void TurnsAllowed()
-        {
-            switch (_boardSize)
-            {
-                case 10:
-                    _turnsAllowed = 17;
-                    break;
-                case 14:
-                    _turnsAllowed = 25;
-                    break;
-                case 18:
-                    _turnsAllowed = 32;
-                    break;
-                default:
-                    _turnsAllowed = 100;
-                    break;
-            }
-        }
-
-        private void FloodFill(int newColor, int oldColor, int row, int col)
-        {
-            if (_boardArray[row, col] != oldColor)
-            {
-                return;
-            }
-            _boardArray[row, col] = newColor;
-            if (row > 0)
-            {
-                FloodFill(newColor, oldColor, row - 1, col);
-            }
-            if (row < _boardSize - 1)
-            {
-                FloodFill(newColor, oldColor, row + 1, col);
-            }
-            if (col > 0)
-            {
-                FloodFill(newColor, oldColor, row, col - 1);
-            }
-            if (col < _boardSize - 1)
-            {
-                FloodFill(newColor, oldColor, row, col + 1);
-            }
-        }
-
+        
+        /// <summary>
+        /// Handles the event of the user clicking on one of the squares of the board.
+        /// Uses flood fill algorithm, redraws board, and checks for if won or lost.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Square_Click(object sender, EventArgs e)
         {
             string[] coords = ((Label)sender).Name.Split(',');
-            int temp = _boardArray[Convert.ToInt32(coords[0]), Convert.ToInt32(coords[1])];
-            if (temp == _boardArray[0, 0])
+            int[,] board = _game.GetBoard();
+            int temp = board[Convert.ToInt32(coords[0]), Convert.ToInt32(coords[1])];
+            if (temp == board[0, 0])
             {
                 return;
             }
             else
             {
-                FloodFill(temp, _boardArray[0, 0], 0, 0);
-                RedrawBoard(); 
+                _game.FloodFill(temp, board[0, 0], 0, 0);
+                RedrawBoard(_game.GetBoard()); 
                 _turn++;
-                uxTurns.Text = "Turns: " + _turn + "/" + _turnsAllowed;
-                if (CheckWin())
+                uxTurns.Text = "Turns: " + _turn + "/" + _game.Turns;
+                if (_game.CheckWin())
                 {
                     uxBoard.Enabled = false;
                     MessageBox.Show("You won!");
                 }
                 else
                 {
-                    if (_turnsAllowed == _turn)
+                    if (_game.Turns == _turn)
                     {
                         uxBoard.Enabled = false;
                         MessageBox.Show("Oh no! You lost.");
@@ -203,35 +185,22 @@ namespace Flood_It_Game
                 }
             }
         }
-
-        private bool CheckWin()
-        {
-            if (_boardArray[_boardSize - 1, _boardSize -1] != _boardArray[0,0])
-            {
-                return false;
-            }
-            for (int x = 0; x < _boardSize; x++)
-            {
-                for (int y = 0; y < _boardSize; y++)
-                {
-                    if (_boardArray[x, y] != _boardArray[0, 0])
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
+        
+        /// <summary>
+        /// Handles when new game button is clicked. Restarts game with values in drop down boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void uxNewGame_Click(object sender, EventArgs e)
         {
+            uxBoard.Visible = false;
             uxBoard.Enabled = true;
             _boardSize = Convert.ToInt32(uxSizeList.Text.Substring(0,2));
-            CreateBoardArray();
+            _game = new Game(_boardSize);
             _turn = 0;
-            TurnsAllowed();
-            uxTurns.Text = "Turns: " + _turn + "/" + _turnsAllowed;
-            DrawBoard();
+            uxTurns.Text = "Turns: " + _turn + "/" + _game.Turns;
+            DrawBoard(_game.GetBoard());
+            uxBoard.Visible = true;
         }
     }
 }
